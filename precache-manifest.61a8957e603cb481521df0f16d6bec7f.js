e1a75a2e1684e59283a84cee34db6f4c33256a1c
iPagar <ipagar@icloud.com>
2019-03-12 21:45:42 +0300

    Updates

---
+++ precache-manifest.61a8957e603cb481521df0f16d6bec7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9ccb53e9fcbb94c742",
+    "url": "/donate-blood/static/js/runtime~main.a18b42a9.js"
+  },
+  {
+    "revision": "78ffc479670251298d7e",
+    "url": "/donate-blood/static/js/main.a8c39198.chunk.js"
+  },
+  {
+    "revision": "966936a0a1f860f84e55",
+    "url": "/donate-blood/static/js/2.d32aaa07.chunk.js"
+  },
+  {
+    "revision": "78ffc479670251298d7e",
+    "url": "/donate-blood/static/css/main.fdfcd892.chunk.css"
+  },
+  {
+    "revision": "966936a0a1f860f84e55",
+    "url": "/donate-blood/static/css/2.8d05eaa2.chunk.css"
+  },
+  {
+    "revision": "9431e2a8a7fa3a8305d185877b2071f3",
+    "url": "/donate-blood/index.html"
+  }
+];

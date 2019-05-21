4dffc0099406f26ee9808492cb2f0e5b58e5279a
iPagar <iPagar@users.noreply.github.com>
2019-05-22 01:07:15 +0300

    Add files via upload

---
+++ precache-manifest.9f11a80cdd934653f1ae4a824ea4f1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9ccb53e9fcbb94c742",
+    "url": "/donate-blood/static/js/runtime~main.a18b42a9.js"
+  },
+  {
+    "revision": "9d43d705c63aaa76d19f",
+    "url": "/donate-blood/static/js/main.78ef61dd.chunk.js"
+  },
+  {
+    "revision": "b587d1df64fc9d4e47e7",
+    "url": "/donate-blood/static/js/2.f1b56fd9.chunk.js"
+  },
+  {
+    "revision": "9d43d705c63aaa76d19f",
+    "url": "/donate-blood/static/css/main.6b1b6d28.chunk.css"
+  },
+  {
+    "revision": "b587d1df64fc9d4e47e7",
+    "url": "/donate-blood/static/css/2.b7fedd49.chunk.css"
+  },
+  {
+    "revision": "aa280e2615c1a86fd3bd2e6e2d123dd8",
+    "url": "/donate-blood/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/donate-blood/precache-manifest.f70d99255b7043d118b7cd12457cd349.js"
+  "/donate-blood/precache-manifest.9f11a80cdd934653f1ae4a824ea4f1de.js"
 );
 
 workbox.clientsClaim();

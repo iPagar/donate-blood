9e63f1571697bfa8198bd94599742b55dd972f0e
Pavel <ipagar@icloud.com>
2021-05-17 16:14:22 +0300

    Updates

---
+++ precache-manifest.2055d8a73ee6afca412c9f51f9c47c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "469e778f0721d08ac7da",
+    "url": "./static/css/main.704f97d4.chunk.css"
+  },
+  {
+    "revision": "469e778f0721d08ac7da",
+    "url": "./static/js/main.00537a65.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "36b2b0481c78296b86ca",
+    "url": "./static/css/2.ba602159.chunk.css"
+  },
+  {
+    "revision": "36b2b0481c78296b86ca",
+    "url": "./static/js/2.1f4e6a5b.chunk.js"
+  },
+  {
+    "revision": "1addf00c51d957c47ee8477f38149af2",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/donate-blood/precache-manifest.897c4e1a2a5e049addfcec075e93ded8.js"
+  "./precache-manifest.2055d8a73ee6afca412c9f51f9c47c38.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/donate-blood/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
